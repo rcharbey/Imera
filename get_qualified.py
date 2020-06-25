0a8caa861d456518a9fb278e236df58f2b6bc65b
Raphaël <raphael.charbey@imt-atlantique.fr>
2020-06-25 17:06:13 +0200

    debug

--- get_qualified.py
+++ get_qualified.py
@@ -84,10 +84,7 @@ if __name__ == '__main__':
 	build_folders(folder)
 	write_README()
 	
-	list_egos =  [x.split('.')[0] for x in listdir(join('..', 'Data', 'Posts'))]
+	list_egos =  [x.split('.')[0] for x in listdir(join('..', 'JSONS'))]
 	for ego in list_egos:
 		Get_qualified(ego).run()
-	
-   
-	
 										    
