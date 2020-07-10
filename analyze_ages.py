389b21d7985bbbc4cb102d7986b5b43158c89ab7
Raphaël <raphael.charbey@imt-atlantique.fr>
2020-07-10 14:58:57 +0200

    test bars

--- analyze_ages.py
+++ analyze_ages.py
@@ -18,37 +18,37 @@ if __name__ == '__main__':
 	list_thresholds = [1.25, 1.5, 1.75, 2.0, 3.0]
 	age_per_ego = get_ages()
 	age_span_per_ego = get_age_span()
-	
-	youngs = [ego for ego in age_per_ego if age_per_ego[ego] < 25]
-	print(f'prop jeunes : {round(len(youngs) / len(age_per_ego), 2)}')
-	
-	list_all_ages = []
-	all_age_min, all_age_max = 1000, 0
-	nb_per_age, nb_churn_per_age = {}, {}
-	for ego in age_span_per_ego:
-		age_min, age_max = age_span_per_ego[ego]
-		age_min = int(age_min.split('.')[0])
-		age_max = int(age_max.split('.')[0])
-		for age in range(age_min, age_max + 1):
-			list_all_ages.append(age)
-			
-			if age < all_age_min:
-				all_age_min = age
-			if age > all_age_max:
-				all_age_max = age
-				
-			if not age in nb_per_age:
-				nb_per_age[age] = 0
-				nb_churn_per_age[age] = 0
-			nb_per_age[age] += 1	
-				
-	list_all_ages.sort()
-	
-	result_folder = join('..', 'Results')
-	plot_folder = join(result_folder, 'Plot_churns')
 		
 	
 	for threshold in list_thresholds:
+	
+		youngs = [ego for ego in age_per_ego if age_per_ego[ego] < 25]
+		print(f'prop jeunes : {round(len(youngs) / len(age_per_ego), 2)}')
+		
+		list_all_ages = []
+		all_age_min, all_age_max = 1000, 0
+		nb_per_age, nb_churn_per_age = {}, {}
+		for ego in age_span_per_ego:
+			age_min, age_max = age_span_per_ego[ego]
+			age_min = int(age_min.split('.')[0])
+			age_max = int(age_max.split('.')[0])
+			for age in range(age_min, age_max + 1):
+				list_all_ages.append(age)
+				
+				if age < all_age_min:
+					all_age_min = age
+				if age > all_age_max:
+					all_age_max = age
+					
+				if not age in nb_per_age:
+					nb_per_age[age] = 0
+					nb_churn_per_age[age] = 0
+				nb_per_age[age] += 1	
+					
+		list_all_ages.sort()
+		
+		result_folder = join('..', 'Results')
+		plot_folder = join(result_folder, 'Plot_churns')
 		
 		churner_age_folder = join(result_folder, 'Churner_ages')
 		list_ages = []
